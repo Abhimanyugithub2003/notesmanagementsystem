528039a90b6d46dadc2dc2b0f1ef2d14d8dfad99
Abhimanyu Singh Parihar <149948798+Abhimanyugithub2003@users.noreply.github.com>
2026-05-13 19:14:21 +0530

    Add files via upload

---
+++ backend project/notes-app/backend/noteroute.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const auth = require("../middleware/authMiddleware");
+
+const {
+    getNotes,
+    createNote,
+    updateNote,
+    deleteNote
+} = require("../controllers/noteController");
+
+router.get("/", auth, getNotes);
+router.post("/", auth, createNote);
+router.put("/:id", auth, updateNote);
+router.delete("/:id", auth, deleteNote);
+
+module.exports = router;
